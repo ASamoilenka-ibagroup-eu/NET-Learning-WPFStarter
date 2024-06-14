8f890c94f59716c2983e3968dd60a3f93f41dddb
Artsiom Samoilenka <ASamoilenka@gomel.iba.by>
2024-06-14 17:18:32 +0300

    Initial Commit

---
+++ WPFStarter/WPFStarter/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WPFStarter.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DataRecord> Records { get; set; }
+    }
+}
---
+++ WPFStarter/WPFStarter/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using WPFStarter.Models.Data;
+
+namespace WPFStarter
+{
+    public partial class App : Application
+    {
+        private readonly IHost host;
+
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(connectionString));
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host.StartAsync();
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await host.StopAsync();
+            host.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
